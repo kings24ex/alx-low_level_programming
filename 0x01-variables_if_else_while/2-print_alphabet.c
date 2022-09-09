4dccd3f349e5e3e236c46b4f32d1591fa05af7fc
kings24ex <kingmsp1@gmail.com>
2022-09-09 15:33:56 -0700

    alphABET

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,16 +7,22 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
+ {
 	char c = 'a';
+	char C = 'A';
 
 	while (c <= 'z')
 	{
-
-	putchar(c);
-	c++;
-
+		putchar(c);
+		c++;
 	}
+	while (C <= 'Z')
+	{
+		
+		putchar(C);
+		C++;
+	
+		}
 	putchar('\n');
 
 	return (0);
