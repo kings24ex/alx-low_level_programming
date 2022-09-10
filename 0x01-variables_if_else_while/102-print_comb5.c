9b47cdc7d89f3f47e7219c76e40a22c2e9264fbb
kings24ex <kingmsp1@gmail.com>
2022-09-10 02:22:43 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- main - main block
+ * main - main block
  *Description: Print all possible combinations of two two-digit numbers.
  *Numbers should range from 0 to 99.
  *The two numbers should be separated by a space.
